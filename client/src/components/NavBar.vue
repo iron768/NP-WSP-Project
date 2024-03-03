5dc811796dd667a9ae1c34db10f073202e66fb9f
CJ <iron768gamer@gmail.com>
2024-03-03 13:57:45 -0500

    Setup basic vite template

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import { ref } from 'vue';
+
+let isActive = ref(false);
+
+function toggleMenu() {
+  isActive.value = !isActive.value;
+}
+
+</script>
+
+<template>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+    </a>
+
+    <a role="button" @click="toggleMenu" :class=" isActive ? `is-active` : `` " class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu" :class=" isActive ? `is-active` : `` " >
+    <div class="navbar-start">
+      <RouterLink to="/" class="navbar-item">
+        Home
+      </RouterLink>
+
+      <RouterLink to="/documentation" class="navbar-item">
+        Documentation
+      </RouterLink>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          More
+        </a>
+
+        <div class="navbar-dropdown">
+          <RouterLink to="/about" class="navbar-item">
+            About
+          </RouterLink>
+          <a class="navbar-item">
+            Jobs
+          </a>
+          <a class="navbar-item">
+            Contact
+          </a>
+          <hr class="navbar-divider">
+          <a class="navbar-item">
+            Report an issue
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light">
+            Log in
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+</template>
+
+<style scoped>
+
+.router-link-active {
+    border-bottom: 2px solid darkcyan;
+}
+
+</style>
---
+++ client/src/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="hero is-primary is-large">
+    <div class="hero-body">
+      <div container="container">
+        <h1 class="title">Home</h1>
+        <h2 class="subtitle">Welcome to the Home page</h2>
+      </div>
+    </div>
+  </main>
+</template>
