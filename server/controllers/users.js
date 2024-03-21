3bb8047cd3a95da4e35437caf03588f364aa5987
CJ <iron768gamer@gmail.com>
2024-03-21 11:11:57 -0400

    Finish express from last class

---
+++ server/controllers/users.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express.Router();
+
+app
+    .get('/', (req, res) => {
+        res.send( [
+            {name: "Moshe", age: 50},
+            {name: "Leah", age: 48},
+        ] )
+    })
+    .get('/test', (req, res) => {
+        res.send({
+            name: "Test User",
+            age: 613
+        })
+    })
+
+module.exports = app
--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const users = require('./controllers/users');
 
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('hello world!!!!');
-});
+  res.send('Hello New Paltz!')
+})
+.use('/users', users)
+
 
 app.listen(PORT, () => {
-    console.log(`App listening at http://localhost:${PORT}`)
+  console.log(`App listening at http://localhost:${PORT}`)
 });
